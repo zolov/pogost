18050b14ae40d893570fbbdc7305bb4dd4bb32b4
zolov <skvrno@gmail.com>
2019-12-26 10:16:11 +0300

    Refactor WebSecConfig and add userservice

--- src/main/java/ru/zolov/pogost/controller/MainController.java
+++ src/main/java/ru/zolov/pogost/controller/MainController.java
@@ -1,11 +1,13 @@
 package ru.zolov.pogost.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.zolov.pogost.domain.Message;
+import ru.zolov.pogost.domain.User;
 import ru.zolov.pogost.repos.MessageRepo;
 import java.util.Map;
 
@@ -30,8 +32,11 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
 
--- src/main/java/ru/zolov/pogost/domain/Message.java
+++ src/main/java/ru/zolov/pogost/domain/Message.java
@@ -1,10 +1,7 @@
 package ru.zolov.pogost.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -13,13 +10,21 @@ public class Message {
     private Integer id;
     private String text;
     private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private User author;
 
     public Message(){}
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        this.author = user;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
     }
 
     public Integer getId() {
